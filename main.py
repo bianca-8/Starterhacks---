0121e842315642b0ad45c0920fe65d77df29fcdc
Bianca <130195861+bianca-8@users.noreply.github.com>
2024-07-27 16:56:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pygame
 
 # set up
 pygame.init()
-screen = pygame.display.set_mode((1440, 1024))
+screen = pygame.display.set_mode((1280, 780))
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -21,12 +21,26 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
+    screen.fill("green")
+    """
+    bg = pygame.image.load("background.png")
+    bg = pygame.transform.scale(bg, (1280, 800))
+    screen.blit(bg, (0, 0))
+    """
+    # background guidelines - delete after
+    for i in range(0, 1280, 80):
+        for j in range(0, 780, 80):
+            pygame.draw.rect(screen, "black", (i, j, 80, 80), 5)
 
+    # characters
     pygame.draw.circle(screen, "red", player_pos, 40)
     pygame.draw.circle(screen, "blue", player_pos2, 40)
 
+    # stick
+    pygame.draw.line(screen,"black", (0,0), (0,80), 5)
+
     keys = pygame.key.get_pressed()
+
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
     if keys[pygame.K_s]:
