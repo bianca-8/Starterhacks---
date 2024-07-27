b92524b0e36cdc15e14e3492550713150d4efff9
Bianca <130195861+bianca-8@users.noreply.github.com>
2024-07-27 15:49:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 # import
 import pygame
 
+#colors
+
+
 # set up
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1440, 1024))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos2 = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # user didn't press quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("green")
 
     pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "blue", player_pos2, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -31,16 +35,14 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
-
-    keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        player_pos.y -= 300 * dt
+        player_pos2.y -= 300 * dt
     if keys[pygame.K_DOWN]:
-        player_pos.y += 300 * dt
+        player_pos2.y += 300 * dt
     if keys[pygame.K_LEFT]:
-        player_pos.x -= 300 * dt
+        player_pos2.x -= 300 * dt
     if keys[pygame.K_RIGHT]:
-        player_pos.x += 300 * dt
+        player_pos2.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
